79225f873aac4e2e07f97e09b97b7b212027bc4c
oliverjam <oliverjamesphillips@gmail.com>
2017-05-17 15:48:03 +0100

    Finished call functionality

--- https/assets/caller.js
+++ https/assets/caller.js
@@ -1,9 +1,9 @@
 /* global document, EndPoint */
 const VideoEndPoint = (function() {
-    /** @class VideoEndPoint
+  /** @class VideoEndPoint
      *  @description Specialisation of the generic EndPoint. Each instance of this class
      *  represents an actual video UI end point.
-     */
+   */
   class VideoEndPoint extends EndPoint {
     constructor(ep_name, remoteVideoTag, localVideoTag, statusTag) {
       super(ep_name);
@@ -20,39 +20,52 @@ const VideoEndPoint = (function() {
     }
 
     makeCall(callTargetName, data) {
-      // Only make a call if not already calling someone else
-      // debugger;
-      if (this._state === 'IDLE') {
-        this.send(callTargetName, 'CALL_REQUEST', data);
-        this.setState('ONTHEPHONE');
-      }
-      //
-      if (this._state === 'IDLE') {
-        this.setState('CALLING');
+      if (EndPoint.names[callTargetName]) {
+        // Only make a call if not already calling someone else
+        if (this._state === 'IDLE') {
+          this._onCallWith = callTargetName;
+          this.setState('CALLING');
+          this.send(callTargetName, 'CALL_REQUEST', data);
+        }
+      } else {
+        console.log('Please enter a valid name');
+        return;
       }
     }
 
     acceptCall() {
-      console.log(this._onCallWith);
       this.send(this._onCallWith, 'ACCEPT_CALL');
+      this.setState('CALLED');
     }
-        /** @method receive
+
+    hangUp() {
+      this.setState('IDLE');
+      this.send(this._onCallWith, 'END_CALL');
+    }
+    /** @method receive
          *  @description Entry point called by the base class when it receives a message for this object from another EndPoint.
          *  @param {String} from - the directory name of the remote EndPoint that sent this request
          *  @param {String} operation - the text string identifying the name of the method to invoke
          *  @param {Object} [data] - the opaque parameter set passed from the remote EndPoint to be sent to the method handler
          */
-        // Provide the required 'receive' method
+    // Provide the required 'receive' method
     receive(from, operation, data) {
-      this.log("END POINT RX PROCESSING... (" + from + ", " + operation + ")", data);
+      this.log(
+        'END POINT RX PROCESSING... (' + from + ', ' + operation + ')',
+        data
+      );
       switch (operation) {
       case 'CALL_REQUEST': {
         this._onCallWith = from;
-        this.setState('CALLED');
-        this.acceptCall();
+        if (this._state === 'IDLE') {
+          this.acceptCall();
+        } else {
+          this.send(this._onCallWith, 'DENIED');
+        }
         break;
       }
       case 'DENIED':
+        this.setState('IDLE');
         break;
       case 'ACCEPT_CALL':
         this.setState('ONTHEPHONE');
@@ -60,21 +73,22 @@ const VideoEndPoint = (function() {
       case 'SDP_OFFER':
         break;
       case 'SDP_ANSWER': {
-        this.send(from, 'SDP_OFFER', {a: 'hey to you too'});
+        this.send(from, 'SDP_OFFER', { a: 'hey to you too' });
         break;
       }
       case 'ICE_CANDIDATE':
         break;
       case 'END_CALL':
+        this.setState('IDLE');
         break;
       }
     }
-        /** @method hangupCall
+    /** @method hangupCall
          *  @description The localEndPoint (THIS) wants to terminate the call. This is generally the result of the user
          *  clicking the hang-up button. We call our local 'endCall' method and then send 'END_CALL' to the remote party.
          */
     hangupCall() {}
-        /** @method startCall
+    /** @method startCall
          *  @description The user wants to make a call to a remote EndPoint (target). This first part of the process
          *  is to send a message to the target to request the call. The remote EndPoint may accept the call by sending
          *  'ACCEPT_CALL' or decline the call by sending 'DENIED'. Nothing happens at our end other than to send the
@@ -84,6 +98,6 @@ const VideoEndPoint = (function() {
          *  @param {String} target - the name of the remote party that we want to start a call with
          */
     startCall(target) {}
-    }
+  }
   return VideoEndPoint;
 })();
--- https/assets/driver.js
+++ https/assets/driver.js
@@ -1,65 +1,86 @@
 /* global document, EndPoint, VideoEndPoint */
-    // Application logic here
+// Application logic here
 
-    const V1RemoteVideoTag = document.querySelector('#V1 .remoteVideo');
-    const V2RemoteVideoTag = document.querySelector('#V2 .remoteVideo');
-    const V3RemoteVideoTag = document.querySelector('#V3 .remoteVideo');
-    const V4RemoteVideoTag = document.querySelector('#V4 .remoteVideo');
+const V1RemoteVideoTag = document.querySelector('#V1 .remoteVideo');
+const V2RemoteVideoTag = document.querySelector('#V2 .remoteVideo');
+const V3RemoteVideoTag = document.querySelector('#V3 .remoteVideo');
+const V4RemoteVideoTag = document.querySelector('#V4 .remoteVideo');
 
-    const V1LocalVideoTag = document.querySelector('#V1 .localVideo');
-    const V2LocalVideoTag = document.querySelector('#V2 .localVideo');
-    const V3LocalVideoTag = document.querySelector('#V3 .localVideo');
-    const V4LocalVideoTag = document.querySelector('#V4 .localVideo');
+const V1LocalVideoTag = document.querySelector('#V1 .localVideo');
+const V2LocalVideoTag = document.querySelector('#V2 .localVideo');
+const V3LocalVideoTag = document.querySelector('#V3 .localVideo');
+const V4LocalVideoTag = document.querySelector('#V4 .localVideo');
 
-    const V1StateTag = document.querySelector('#V1 .state');
-    const V2StateTag = document.querySelector('#V2 .state');
-    const V3StateTag = document.querySelector('#V3 .state');
-    const V4StateTag = document.querySelector('#V4 .state');
+const V1StateTag = document.querySelector('#V1 .state');
+const V2StateTag = document.querySelector('#V2 .state');
+const V3StateTag = document.querySelector('#V3 .state');
+const V4StateTag = document.querySelector('#V4 .state');
 
-    const V1 = new VideoEndPoint('V1', V1RemoteVideoTag, V1LocalVideoTag, V1StateTag);
-    const V2 = new VideoEndPoint('V2', V2RemoteVideoTag, V2LocalVideoTag, V2StateTag);
-    const V3 = new VideoEndPoint('V3', V3RemoteVideoTag, V3LocalVideoTag, V3StateTag);
-    const V4 = new VideoEndPoint('V4', V4RemoteVideoTag, V4LocalVideoTag, V4StateTag);
+const V1 = new VideoEndPoint(
+  'V1',
+  V1RemoteVideoTag,
+  V1LocalVideoTag,
+  V1StateTag
+);
+const V2 = new VideoEndPoint(
+  'V2',
+  V2RemoteVideoTag,
+  V2LocalVideoTag,
+  V2StateTag
+);
+const V3 = new VideoEndPoint(
+  'V3',
+  V3RemoteVideoTag,
+  V3LocalVideoTag,
+  V3StateTag
+);
+const V4 = new VideoEndPoint(
+  'V4',
+  V4RemoteVideoTag,
+  V4LocalVideoTag,
+  V4StateTag
+);
 
-    const callBtns = document.querySelectorAll('.startCall');
-    const hangUpBtns = document.querySelectorAll('.endCall');
-    const remoteVideoTags = document.querySelectorAll('.remoteVideo');
-    const localVideoTags = document.querySelectorAll('.localVideo');
+const callBtns = document.querySelectorAll('.startCall');
+const hangUpBtns = document.querySelectorAll('.endCall');
+const remoteVideoTags = document.querySelectorAll('.remoteVideo');
+const localVideoTags = document.querySelectorAll('.localVideo');
 
-    const getInput = (button) => {
-      return button.parentElement.querySelector('.target').value;
-    };
+const getInput = button => {
+  return button.parentElement.querySelector('.target').value;
+};
 
-    const getCallerId = (button) => {
-      return button.parentElement.id;
-    };
+const getCallerId = button => {
+  return button.parentElement.id;
+};
 
-    callBtns.forEach((button)=>{
-      button.addEventListener('click', ()=>{
-        const callTargetName = getInput(button);
-        const callerId = getCallerId(button);
-        // console.log(callTargetName);
-        EndPoint.names[callerId].makeCall(callTargetName, 'Hi Oli');
-      });
-    });
+callBtns.forEach(button => {
+  button.addEventListener('click', () => {
+    const callTargetName = getInput(button);
+    const callerId = getCallerId(button);
+    // console.log(callTargetName);
+    EndPoint.names[callerId].makeCall(callTargetName, 'Hi Oli');
+  });
+});
 
-    hangUpBtns.forEach((button)=>{
-      button.addEventListener('click', ()=>{
-        const callTargetName = getInput(button);
-        const callerId = getCallerId(button);
-      });
-    });
+hangUpBtns.forEach(button => {
+  button.addEventListener('click', () => {
+    const callTargetName = getInput(button);
+    const callerId = getCallerId(button);
+    EndPoint.names[callerId].hangUp();
+  });
+});
 
-    // V1.send('V2', 'SDP_ANSWER', {a: 'yo'});
+// V1.send('V2', 'SDP_ANSWER', {a: 'yo'});
 
-    // const videoTag = document.getElementById('videoTag');
+// const videoTag = document.getElementById('videoTag');
 
-  //   if (videoTag) {
-  //     navigator.mediaDevices.getUserMedia({audio: true, video: true})
-  // .then(function (mediaStream) {
-  //   videoTag.srcObject = mediaStream;
-  // })
-  // .catch(function (err) {
-  //   console.log('video not working', err);
-  // });
-  //   }
+//   if (videoTag) {
+//     navigator.mediaDevices.getUserMedia({audio: true, video: true})
+// .then(function (mediaStream) {
+//   videoTag.srcObject = mediaStream;
+// })
+// .catch(function (err) {
+//   console.log('video not working', err);
+// });
+//   }
